d4074ebce56b35daeee43eb85c36490a449748ed
Song Yao zhu <songyaozhu@Songs-MacBook-Pro.local>
2026-05-30 13:07:10 +0800

    Implement saved locations API

---
+++ backend/repositories/save_repository.py
@@ -0,0 +1,48 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models.saved_locations import Saved_Location
+from models.locations import Location
+
+async def get_user_saves(session, user_id):
+    statement = select(Saved_Location, Location).join(Location, Saved_Location.location_id == Location.id).where(Saved_Location.user_id == user_id)
+
+    result = await session.execute(statement)
+    return result.all()
+
+async def add_user_save(session, user_id, location_id, purpose):
+    statement = select(Location).where(Location.id == location_id)
+
+    result = await session.execute(statement)
+    location : Location = result.scalar_one_or_none()
+
+    if location is None:
+        return None
+
+    new_save = Saved_Location(
+        user_id=user_id,
+        location_id=location.id,
+        purpose=purpose or "",
+    )
+    
+    session.add(new_save)
+    await session.commit()
+    await session.refresh(new_save)
+
+    return new_save
+
+async def delete_user_save(session, user_id, save_id):
+    statement = select(Saved_Location).where(
+            Saved_Location.id == save_id,
+            Saved_Location.user_id == user_id,
+        )
+
+    result = await session.execute(statement)
+    saved_location = result.scalar_one_or_none()
+    if saved_location is None:
+        return False
+    
+    await session.delete(saved_location)
+    await session.commit()
+
+    return True
--- backend/routers/saves.py
+++ backend/routers/saves.py
@@ -1,5 +1,10 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from schemas import save
+from services import save_service
+from core import database
+
 
 router=APIRouter(
     prefix="/users/{user_id}/saves",
@@ -7,21 +12,33 @@ router=APIRouter(
 )
 
 @router.get("", response_model=list[save.SaveResponse])
-def get_user_saves(user_id: str):
-    return []
+async def get_user_saves(user_id: str, session: AsyncSession = Depends(database.get_db_session)):
+    saves = await save_service.get_user_saves(session, user_id)
+    return saves
 
 @router.post("", response_model=save.SaveResponse)
-def add_user_save(user_id: str, request: save.AddSaveRequest):
-    #search data base for fav and then add it ig
-    return {}
+async def add_user_save(user_id: str, request: save.AddSaveRequest, session: AsyncSession = Depends(database.get_db_session)):
+    save = await save_service.add_user_save(session, user_id, request.location_id, request.purpose)
+
+    if save is None:
+        raise HTTPException(status_code=404, detail="not valid location")
+    return save
 
 @router.patch("/{save_id}")
-def update_user_save(user_id: str, save_id: str):
+async def update_user_save(user_id: str, save_id: int, session: AsyncSession = Depends(database.get_db_session)):
     #search data base and update
     return {}
 
 @router.delete("/{save_id}")
-def delete_user_save(user_id: str, save_id: str):
+async def delete_user_save(user_id: str, save_id: int, session: AsyncSession = Depends(database.get_db_session)):
     #same as add search and delete
-    return {}
+    boolean = await save_service.delete_user_save(session, user_id, save_id)
+    if boolean:
+        return {
+            "save deleted": True
+        }
+    else:
+        return {
+            "saved deleted": False
+        }
 
