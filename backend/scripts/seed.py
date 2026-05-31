cbaf46b9bb5105890b464b8e1598e57078a29424
isaaaxe <isaacchoong1901@gmail.com>
2026-05-31 14:42:19 +0800

    Fixed seed.py pt2

--- backend/scripts/seed.py
+++ backend/scripts/seed.py
@@ -73,6 +73,8 @@ async def clear_existing_data(session):
     await session.execute(Map_Edge.__table__.delete())
     await session.execute(Location.__table__.delete())
     await session.execute(Map_Node.__table__.delete())
+    await session.execute(User.__table__.delete())
+    await session.execute(Saved_Location.__table__.delete())
 
 
 async def seed_data(clear_first: bool = True):
